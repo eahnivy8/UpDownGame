d6216603d85ccbb567efe37df9a765e5d15d8929
Eddie <eahnivy8@gmail.com>
2019-08-30 13:25:19 +0900

    UpDown Game Completed

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // properties
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var turnCountLabel: UILabel!
+    @IBOutlet weak var inputField: UITextField!
+    
+    var randomNumber: UInt32 = 0
+    var turnCount: Int = 0
+    
+    @IBAction func touchUpSubmitButton(_ sender: UIButton) {
+        guard let inputText = self.inputField.text, inputText.isEmpty == false else {
+            print("No Input Number")
+            return
+        }
+        guard let inputNumber: UInt32 = UInt32(inputText) else {
+            print("Wrong Input Number")
+            return
+        }
+        turnCount += 1
+        self.turnCountLabel.text = "\(turnCount)"
+        if inputNumber > randomNumber {
+            self.resultLabel.text = "UP!"
+        } else if inputNumber < randomNumber {
+            self.resultLabel.text = "DOWN!"
+        } else {
+            self.resultLabel.text = "Correct!"
+        }
+    }
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        self.initializeGame()
+        }
+    
+    @IBAction func tapBackground(_ sender: UITapGestureRecognizer) {
+        self.inputField.resignFirstResponder()
+    }
+    // Mark: Custom Methods
+    func initializeGame() {
+        
+        self.randomNumber = arc4random() % 25
+        self.turnCount = 0
+        
+        self.resultLabel.text = "Start!"
+        self.turnCountLabel.text = "\(turnCount)"
+        self.inputField.text = nil
+        
+        print("Random Number \(self.randomNumber)")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.initializeGame()
     }
-
 
 }
 
